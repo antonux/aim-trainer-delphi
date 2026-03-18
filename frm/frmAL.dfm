a7fdbd96ecca52314233c7f49ffe21fb4a8ac40c
antonux <marcusremigoso@gmail.com>
2026-03-19 01:14:24 +0800

    exe ready

--- frm/frmAL.dfm
+++ frm/frmAL.dfm
@@ -10,8 +10,9 @@ object AL: TAL
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
-  Position = poDesigned
+  Position = poScreenCenter
   RoundedCorners = rcOn
+  OnCreate = FormCreate
   DesignSize = (
     1116
     615)
@@ -26,10 +27,97 @@ object AL: TAL
     Color = 1842720
     ParentBackground = False
     TabOrder = 0
+    object Label1: TLabel
+      Left = 24
+      Top = 16
+      Width = 42
+      Height = 21
+      Caption = 'Score:'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWhite
+      Font.Height = -16
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object Label2: TLabel
+      Left = 128
+      Top = 16
+      Width = 35
+      Height = 21
+      Caption = 'Miss:'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWhite
+      Font.Height = -16
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object lSc: TLabel
+      Left = 72
+      Top = 16
+      Width = 9
+      Height = 21
+      Caption = '0'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clLime
+      Font.Height = -16
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object lMi: TLabel
+      Left = 169
+      Top = 16
+      Width = 9
+      Height = 21
+      Caption = '0'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = 8388863
+      Font.Height = -16
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object cbDiff: TComboBox
+      Left = 358
+      Top = 16
+      Width = 145
+      Height = 23
+      Hint = 'Difficulty'
+      ParentCustomHint = False
+      Color = 4194304
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWhite
+      Font.Height = -12
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ItemIndex = 0
+      ParentFont = False
+      ParentShowHint = False
+      ShowHint = True
+      TabOrder = 0
+      Text = 'Easy'
+      OnChange = cbDiffChange
+      Items.Strings = (
+        'Easy'
+        'Normal'
+        'Hard'
+        'Hell')
+    end
+    object bSt: TButton
+      Left = 232
+      Top = 16
+      Width = 75
+      Height = 24
+      Caption = 'Start'
+      TabOrder = 1
+      OnClick = bStClick
+    end
   end
   object sb: TScrollBox
-    Left = 144
-    Top = 155
+    Left = 128
+    Top = 171
     Width = 844
     Height = 215
     Anchors = [akLeft, akRight]
@@ -38,11 +126,19 @@ object AL: TAL
     BevelOuter = bvNone
     BorderStyle = bsNone
     Color = 2961202
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -12
+    Font.Name = '='
+    Font.Style = []
     ParentColor = False
+    ParentFont = False
     TabOrder = 1
   end
-  object Timer1: TTimer
-    OnTimer = Timer1Timer
+  object tmr: TTimer
+    Enabled = False
+    Interval = 2000
+    OnTimer = tmrTimer
     Left = 904
     Top = 88
   end
--- frm/frmAL.pas
+++ frm/frmAL.pas
@@ -4,24 +4,37 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
 
 type
   TAL = class(TForm)
-    Timer1: TTimer;
+    tmr: TTimer;
     Panel1: TPanel;
     sb: TScrollBox;
-    procedure Timer1Timer(Sender: TObject);
+    Label1: TLabel;
+    Label2: TLabel;
+    lSc: TLabel;
+    lMi: TLabel;
+    cbDiff: TComboBox;
+    bSt: TButton;
+    procedure tmrTimer(Sender: TObject);
+    procedure cbDiffChange(Sender: TObject);
+    procedure bStClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     procedure pnlClick(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
     procedure spawnPanel;
+    procedure init;
+    procedure refresh;
+    procedure renderHitbox(var w,h: integer);
   public
     { Public declarations }
   end;
 
 var
   AL: TAL;
+  ttlSc: integer;
 
 implementation
 
@@ -29,27 +42,104 @@ implementation
 
 { TAL }
 
+procedure TAL.FormCreate(Sender: TObject);
+begin
+  init;
+end;
+
+procedure TAL.bStClick(Sender: TObject);
+begin
+ //
+  if upperCase(bSt.Caption)='START' then
+  begin
+    bSt.Caption:= 'Stop';
+    tmr.Enabled:= true;
+  end
+  else
+  begin
+    bSt.Caption:= 'Start';
+    tmr.Enabled:= false;
+  end;
+  init;
+end;
+
+procedure TAL.cbDiffChange(Sender: TObject);
+begin
+  refresh;
+  case cbDiff.ItemIndex of
+  0: tmr.Interval:= 2000;
+  1: tmr.Interval:= 1000;
+  2: tmr.Interval:= 750;
+  3: tmr.Interval:= 450;
+  end;
+end;
+
+
+
+procedure TAL.init;
+begin
+  lSc.Caption := '0';
+  lMi.Caption := '0';
+  sb.DestroyComponents;
+  ttlSc:= 0;
+end;
+
 procedure TAL.pnlClick(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 var
   shp: TShape;
 begin
-  shp := Sender as TShape;
-  shp.Brush.Color := clRed;
+  shp := (sender as TShape);
+  if shp.Tag = 0 then
+  begin
+    shp.Brush.Color := clRed;
+    shp.Tag := 1;
+    lSc.Caption := IntToStr(StrToInt(lSc.Caption)+1);
+  end;
 end;
 
 
+procedure TAL.refresh;
+begin
+  bSt.Caption:= 'Start';
+  tmr.Enabled:= false;
+  init;
+end;
+
+procedure TAL.renderHitbox(var w,h: integer);
+begin
+  case cbDiff.ItemIndex of
+  0: begin
+       w := 35;
+       h := 35;
+     end;
+  1: begin
+       w := 30;
+       h := 30;
+     end;
+  2: begin
+       w := 25;
+       h := 25;
+     end;
+  3: begin
+       w := 20;
+       h := 20;
+     end;
+  end;
+end;
+
 procedure TAL.spawnPanel;
 var
-  x,y: integer;
+  x,y,w,h: integer;
 begin
   randomize;
-
+  renderHitbox(w,h);
   with TShape.Create(sb) do
   begin
     Parent := sb;
-    Width := 35;
-    Height := 35;
+    tag:= 0;
+    Width := w;
+    Height := h;
     Shape := stEllipse;
     Brush.Color := clBlue;
     Pen.Color := clBlack;
@@ -59,11 +149,27 @@ begin
     top:= y;
     left:= x;
   end;
+  inc(ttlSc);
 end;
 
-procedure TAL.Timer1Timer(Sender: TObject);
+procedure TAL.tmrTimer(Sender: TObject);
+var
+  shp: TControl;
 begin
+
+  if sb.ComponentCount>0 then
+  begin
+    if (sb.Controls[0] as TShape).Tag=0 then
+      lMi.Caption := IntToStr(StrToInt(lMi.Caption)+1);
+  end;
+
   sb.DestroyComponents;
+  if ttlSc=30 then
+  begin
+    tmr.Enabled:= false;
+    bSt.Caption:= 'Start';
+    exit;
+  end;
   spawnPanel;
 end;
 
